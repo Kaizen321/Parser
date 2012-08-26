8a3f61a0f822becbb01f717d0c44d370d067cc0b
Jose Gonzalez <jagc1982@gmail.com>
2012-08-25 21:32:11 -0400

    Initial commit for word parser

---
+++ Parser/WordParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser
+{
+    public class WordParser
+    {
+        public IList<string> GetUniqueWords(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                return new List<string>();
+
+            content = RemoveSpecialCharacters(content.Trim());
+            return content.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                          .Distinct(StringComparer.CurrentCultureIgnoreCase).ToList();
+        }
+
+        public IList<string> GetUniqueWordsInAlphabeticalOrder(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                return new List<string>();
+
+            var uniqueWords = GetUniqueWords(content).ToList();
+            if (uniqueWords.Count() == 0)
+                return uniqueWords;
+
+            uniqueWords.Sort(String.Compare);
+            return uniqueWords;
+        }
+
+        public IList<string> RemoveIgnoreWords(IList<string> words, IList<string> ignoreWords)
+        {
+            if (ignoreWords == null || ignoreWords.Count() == 0)
+                return words;
+
+            return words.Where(word => !ignoreWords.Contains(word.ToLower())).ToList();            
+        }
+
+        private string RemoveSpecialCharacters(string content)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in content)
+            {
+                if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == ' ')
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
